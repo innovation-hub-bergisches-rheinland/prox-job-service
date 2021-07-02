902cb0cdfa2523b5c89cc617b3b570529b064f37
Tobias Hund <tobias.hund@innovation-hub.de>
2021-07-02 12:19:33 +0200

    Initial commit

---
+++ src/main/java/de/innovationhub/prox/jobservice/ProxJobServiceApplication.java
@@ -0,0 +1,13 @@
+package de.innovationhub.prox.jobservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxJobServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProxJobServiceApplication.class, args);
+  }
+
+}
