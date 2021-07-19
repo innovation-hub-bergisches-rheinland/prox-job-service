ef0d7f033139d55fc62d2b5fb3873a630e8049e8
Tobias Hund <tobias.hund@innovation-hub.de>
2021-07-19 16:09:33 +0200

    Remove descritpions from enum and add local profile

--- src/main/java/de/innovationhub/prox/jobservice/domain/job/JobEntryLevel.java
+++ src/main/java/de/innovationhub/prox/jobservice/domain/job/JobEntryLevel.java
@@ -1,20 +1,7 @@
 package de.innovationhub.prox.jobservice.domain.job;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-
 public enum JobEntryLevel {
-  CAREER_STARTER("Berufseinsteiger"),
-  EXPERIENCED("Berufserfahrene")
+  CAREER_STARTER, // Berufseinsteiger
+  EXPERIENCED, // Berufserfahrene
   ;
-
-  private String description;
-
-  JobEntryLevel(String description) {
-    this.description = description;
-  }
-
-  public String getDescription() {
-    return description;
-  }
 }
--- src/main/java/de/innovationhub/prox/jobservice/domain/job/JobType.java
+++ src/main/java/de/innovationhub/prox/jobservice/domain/job/JobType.java
@@ -4,20 +4,10 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 public enum JobType {
-  FULL_TIME("Vollzeit"),
-  PART_TIME("Teilzeit"),
-  INTERNSHIP("Ausbildung / Praktikum"),
-  MINIJOB("Minijob"),
-  STUDENT_WORKER("Studentische Hilfskraft / Werkstudent"),
-  RESEARCH_ASSISTANT("Wissenschaftliche Hilfskraft");
-
-  private String description;
-
-  JobType(String description) {
-    this.description = description;
-  }
-
-  public String getDescription() {
-    return description;
-  }
+  FULL_TIME, // Vollzeit
+  PART_TIME, // Teilzeit
+  INTERNSHIP, // Ausbildung / Praktikum
+  MINIJOB, // Minijob
+  STUDENT_WORKER, // Studentische Hilfskraft / Werkstudent
+  RESEARCH_ASSISTANT; // Wissenschaftliche Hilfskraft
 }
